8f4bbe3f3f963f5fadfdcdb89b200c304f7f7bbf
Robin.K <91210322+krobingit@users.noreply.github.com>
2021-11-30 01:54:37 +0530

    Update mongodb-task1.js

--- mongodb-task1.js
+++ mongodb-task1.js
@@ -29,33 +29,39 @@ db.products.find({ product_price: { $gte: 500 } }).limit(4);
 
 
 //Find the product name and product material of each products-----------------------
+
 db.products.find({},{_id:0,product_name:1,product_material:1});
 
 
 
 
-//Find the product with a row id of 10
+//Find the product with a row id of 10-----------------------
+
 db.products.find({ id: "10" })
 
 
 
-//Find only the product name and product material
+//Find only the product name and product material-------------------
+
 db.products.find({ id: "10" }, { product_name: 1, product_material: 1, _id: 0 });
 
 
 
 
-//Find all products which contain the value of soft in product material
+//Find all products which contain the value of soft in product material---------------------
+
 db.products.find({product_material:"Soft"})
 
 
 
 
-//Find products which contain product color indigo  and product price 492.00
+//Find products which contain product color indigo  and product price 492.00-----------------------------------
+
 db.products.find({ product_price: 492, product_color: "indigo" })
 
 
 
 
-//Delete the products which product price value are same
+//Delete the products which product price value are same---------------------------------------
+
 db.products.aggregate([{ $group: { _id: "$product_price", count: { $count: {} } } }, { $match: { count: { $gt: 1 } } }]).forEach(document => db.products.deleteMany({product_price:document._id}));
